f2a57413ec432147540bc3e6bab8c7387f4bb920
afroprogrammer <forsonfranklin@gmail.com>
2020-06-02 00:09:38 +0000

    Using the Test Method  challenge completed

---
+++ using-the-test-method.js
@@ -0,0 +1,11 @@
+//Apply the regex myRegex on the string myString using the .test() method.
+
+/**HINT** 
+You should use .test() to test the regex.
+
+Your result should return true.
+*/
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
