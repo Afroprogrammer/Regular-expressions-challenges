bbab3f55e3e25dd7e9bc43013e272918e085225a
afroprogrammer <forsonfranklin@gmail.com>
2020-06-04 23:23:01 +0000

    Positive and Negative Lookahead Challenge completed

---
+++ positive-and-negative-lookahead.js
@@ -0,0 +1,17 @@
+/**
+let quit = "qu";
+let noquit = "qt";
+let quRegex= /q(?=u)/;
+let qRegex = /q(?!u)/;
+quit.match(quRegex); // Returns ["q"]
+noquit.match(qRegex); // Returns ["q"]
+A more practical use of lookaheads is to check two or more patterns in one string. Here is a (naively) simple password checker that looks for between 3 and 6 characters and at least one number:
+
+let password = "abc123";
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+checkPass.test(password); // Returns true
+ */
+
+let sampleWord = "astronaut";
+let pwRegex = /^\D(?=\w{5})(?=\w*\d{2})/i; // Change this line;
+let result = pwRegex.test(sampleWord);
