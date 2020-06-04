1dc62cc25e0cc46d00c07f5481f0c636bc50c345
afroprogrammer <forsonfranklin@gmail.com>
2020-06-04 22:59:22 +0000

    Check for All or None Challenge completed

---
+++ check-for-all-or-none.js
@@ -0,0 +1,13 @@
+/**
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american); // Returns true
+rainbowRegex.test(british); // Returns true
+ */
+
+//Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
